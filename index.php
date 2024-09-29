ba251118dbba29d9f694e2030481c516660cc9b7
Diane Binsztok <diane.binsztok@le-campus-numerique.fr>
2024-09-29 15:06:15 +0200

    feat: basic routing

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
-require "single-card.php";
+define('REQUEST_URI', str_replace(dirname($_SERVER['SCRIPT_NAME']), '', $_SERVER['REQUEST_URI']));
+
+
+
+switch (REQUEST_URI) {
+    case '/':
+        require "homepage.php";
+        break;
+
+    case '/card':
+        require "single-card.php";
+        break;
+
+    default:
+        echo "La page n'existe pas";
+}
